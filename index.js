29232f17b34ea07eb2d0b879ceb9e5e674360cd7
Fabubakar24 <abubakar.all.dv@gmail.com>
2021-11-11 00:39:16 +0600

    some change for time zoon

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ async function run() {
 // Get appointments info from database 
         app.get('/appointments', verifyToken, async (req, res) => {
             const email = req.query.email;
-            const date = new Date(req.query.date).toLocaleDateString();
+            const date = req.query.date;
             const query = { patientEmail: email, date: date };
             const cursor = appointmentsCollection.find(query);
             const appointments = await cursor.toArray();
